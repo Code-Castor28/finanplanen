02be858f691449a629061f31704d7b559a58a7bc
CodeAriCos28 <arafaelacosta28@gmail.com>
2026-06-10 21:56:19 -0400

    feat: implement modular expense, transfer, and savings management system with HTMX integration

--- apps/savings/views.py
+++ apps/savings/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+
+class AhorroLista(LoginRequiredMixin, TemplateView):
+    template_name = 'savings/ahoro.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['section'] = 'savings'
+        return context
